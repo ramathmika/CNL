51c18a0b66c4c8df5b3437becf013f2e5e6a5b28
ramathmika <35446516+ramathmika@users.noreply.github.com>
2019-10-03 11:20:33 +0530

    Add files via upload

---
+++ CNL/LAB10/40.tcl
@@ -0,0 +1,56 @@
+set ns [new Simulator]
+
+
+set nt [open simulate.nam w]
+$ns namtrace-all $nt
+
+set tr [open udp-40.tr w]
+$ns trace-all $tr
+
+proc finish {} {
+	global ns nt tr
+	$ns flush-trace
+	close $nt
+	close $tr  
+	exec nam simulate.nam &
+	exec awk -f measure-loss.awk udp-40.tr &
+	# exec awk -f PacketLoss.awk test12.tr > udp-40.tr &
+	# exec xgraph udp-40.tr &
+	exit 0
+}
+
+set S [$ns node]
+set R [$ns node]
+set D [$ns node]
+
+$ns duplex-link $S $R 1Mb 50ms SFQ
+$ns duplex-link $R $D 100Kb 5ms SFQ
+
+$ns queue-limit $R $D 10
+
+$ns duplex-link-op $S $R orient right
+$ns duplex-link-op $R $D orient right
+
+$ns duplex-link-op $S $R queuePos 1.0
+$ns duplex-link-op $R $D queuePos 1.0
+
+set udp [new Agent/UDP]
+$ns attach-agent $S $udp
+
+set null [new Agent/Null]
+$ns attach-agent $D $null
+$ns connect $udp $null
+
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$cbr set type_ CBR
+$cbr set packet_size_ 1000
+$cbr set rate_ 40kb
+$cbr set random_ false
+
+$ns at 0.1 "$cbr start"
+$ns at 10.0 "$cbr stop"
+
+$ns at 10.1 "finish"
+
+$ns run
